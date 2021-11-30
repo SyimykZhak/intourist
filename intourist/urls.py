c36cce7f19a395255a4fc996d95a97084d9de582
Syimyk Zhak <zhakshybaevv@gmail.com>
2021-12-01 00:18:31 +0300

    add media and static setings

--- intourist/urls.py
+++ intourist/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import Settings, settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('places/', include('places.urls')), #с помощью include мы путь отправляем в places приложение 
     path('', include('core.urls')),
-]
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
