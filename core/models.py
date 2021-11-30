81739293be3cc3afa14d19a591e2e8cdd4cd72db
Syimyk Zhak <zhakshybaevv@gmail.com>
2021-11-30 18:29:12 +0300

    add profle

--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import NullBooleanField
+from django.db.models.fields.files import ImageField
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        to=User, on_delete=models.CASCADE,
+        related_name='profile'
+    )
+    region = models.CharField(max_length=255,
+    choices=(
+        ('O','Osh'),
+        ('B','Bishkek'),
+        ('N','Naryn'),
+        ('A','Alai'),
+        ('T','Talas'),
+        ('J','Jalal-Abad'),
+    ))
+    
+    photo = ImageField(
+        upload_to='profile_photo',
+        null=True,blank=True
+    )
+
+    def __str__(self):
+        return self.user.username
+
+
+
 
 # Create your models here.
